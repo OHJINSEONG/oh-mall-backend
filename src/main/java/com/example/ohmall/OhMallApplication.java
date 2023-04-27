32e0fad5d5fb7fa1aedec1800caf856c9c0606cb
OHJINSEONG <ojseong0828@naver.com>
2023-04-28 08:18:29 +0900

    Fix: .gitignore 수정
    .gradle/
    .idea/
    코드 추가

---
+++ src/main/java/com/example/ohmall/OhMallApplication.java
@@ -0,0 +1,34 @@
+package com.example.ohmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class OhMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OhMallApplication.class, args);
+	}
+	//http://localhost:8000/h2-console
+
+	@Bean
+	public WebSecurityCustomizer ignoringCustomizer() {
+		return (web) -> web.ignoring().antMatchers("/**");
+	}
+
+	@Bean
+	public WebMvcConfigurer webMvcConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+			}
+		};
+	}
+
+}
---
+++ src/test/java/com/example/ohmall/OhMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ohmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
